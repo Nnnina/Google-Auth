870875917ab82ceef9083524e78c95350dfcd5d5
Nnnina <nnninalei@gmail.com>
2018-03-01 15:41:22 -0500

    added billing and client side app

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    //express will serve up production assets, like main.js or main.css
+    app.use(express.static('client/build'));
+    //Express will serve up index.html file if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 //connect to mongoDB
 mongoose.connect(key.mongoURI);
 
